18bf81eb8d3f9d12b6a5966e389a32cfac8f12ec
Hayson <heyson@sina.cn>
2017-06-19 17:41:40 +0800

    config initializer qiniu host 改成华南

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
+  if Rails.env.production?
   storage :qiniu
-  # storage :fog
+  elsif Rails.env.development?
+  storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
