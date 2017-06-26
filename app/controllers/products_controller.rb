d878764d6736c71e987239e28df76c4de5232007
Hayson <heyson@sina.cn>
2017-06-27 00:11:21 +0800

    add Ajax to add to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,8 @@ class ProductsController < ApplicationController
   else
     flash[:warning] = "你的购物车内已有此物品"
   end
-    redirect_to :back
+    # redirect_to :back
+    render "add_to_cart"
     end
 
 end
